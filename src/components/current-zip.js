559c47a43462ba5d78734c95d389ad10d813d504
bevanger <baileyevanger@gmail.com>
2021-10-08 17:16:38 -0600

    created current-zip page

---
+++ src/components/current-zip.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import './App.css'
+import './index.css'
+
+const currentZipData = {
+    zip_code: ''
+}
+
+const CurrentZip = () => {
+    const [ zipData, setZipData ] = useState(currentZipData)
+
+    const onSubmit = evt => {
+        evt.preventDefault()
+    };
+
+    const onChange = evt => {
+        const { name, value } = evt.target;
+        setZipData({...zipData, [name]:value});
+        console.log(zipData)
+    };
+
+    return(
+        <div className='currentZipDiv'>
+            <h1 className='currentZipHeader'> Enter Your Current Zip Code </h1>
+            <form className='currentZipForm' onSubmit={onSubmit}>
+                <input
+                    className='currentZipInput'
+                    type='text'
+                    name='zip_code'
+                    placeholder='Zip Code'
+                    value={zipData.zip_code}
+                    onChange={onChange}
+                />
+            </form>
+        </div>
+    )
+};
+
+export default CurrentZip
+
