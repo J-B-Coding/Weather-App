384c373a4104bb8e683d54804f6c04c595defd60
JoeyMBrown <brnbunch45@yahoo.com>
2021-10-08 18:46:27 -0400

    Added basic dummy data

---
+++ src/DummyData/dummyData.js
@@ -0,0 +1,7 @@
+export const dummydata = {
+    weatherType: 'Chilly',
+    temperature: ['17 F', '19 F', '23 F', '15 F', '19 F', '22 F', '12 F'],
+    day: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    location: 'Ohio',
+    humidity: '12%'
+}
